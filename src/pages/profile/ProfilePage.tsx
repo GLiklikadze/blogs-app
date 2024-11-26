786c941250e3396772808c8f1049d75aecbc7ebe
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2024-11-26 15:45:16 +0400

    use react-hook-form in ProfilePage

--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -7,13 +7,14 @@ import { logOut } from "@/supabase/auth/httpRegister";
 import { fillProfileInfo, getProfileInfo } from "@/supabase/profile/profile";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { UserRoundPen } from "lucide-react";
-import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createAvatar } from "@dicebear/core";
 import { avataaars } from "@dicebear/collection";
 import { TabsList, Tabs, TabsTrigger } from "@/components/ui/tabs";
 import { TabsContent } from "@radix-ui/react-tabs";
 import { useTranslation } from "react-i18next";
+import { Controller, useForm } from "react-hook-form";
 
 const initialPayload = {
   full_name: "",
@@ -21,13 +22,25 @@ const initialPayload = {
   avatar_url: "",
   phone_number: "",
 };
+type ProfileFormValues = {
+  full_name: string;
+  full_name_ka: string;
+  avatar_url: string;
+  phone_number: string;
+};
 const ProfilePage = () => {
-  const [profilePayload, setProfilePayload] = useState(initialPayload);
   const [toggleEdit, setToggleEdit] = useState(false);
-  const { full_name, full_name_ka, avatar_url, phone_number } = profilePayload;
+
   const { user } = useAuthContext();
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const { control, handleSubmit, reset, watch } = useForm<ProfileFormValues>({
+    defaultValues: initialPayload,
+  });
+  const full_name = watch("full_name");
+  const full_name_ka = watch("full_name_ka");
+  const avatar_url = watch("avatar_url");
+  const phone_number = watch("phone_number");
 
   const { data: receivedProfileData } = useQuery({
     queryKey: ["getprofileinfo", user?.id],
@@ -37,15 +50,15 @@ const ProfilePage = () => {
 
   useEffect(() => {
     if (receivedProfileData) {
-      setProfilePayload((prevProfilePayload) => ({
-        ...prevProfilePayload,
+      reset((prev) => ({
+        ...prev,
         full_name: receivedProfileData?.full_name ?? "",
         full_name_ka: receivedProfileData?.full_name_ka ?? "",
         avatar_url: receivedProfileData?.avatar_url ?? "",
         phone_number: receivedProfileData?.phone_number ?? "",
       }));
     }
-  }, [receivedProfileData, setProfilePayload]);
+  }, [receivedProfileData, reset]);
 
   const { mutate: mutateLogout } = useMutation({
     mutationKey: ["logOut"],
@@ -66,34 +79,43 @@ const ProfilePage = () => {
   const handleToggleEdit = () => {
     setToggleEdit((prevToggleEdit) => !prevToggleEdit);
   };
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = e.target;
-    setProfilePayload((prevProfileData) => {
-      return {
-        ...prevProfileData,
-        [name]: value,
-      };
-    });
-  };
-  const handleSaveInfo = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const { value, name } = e.target;
+  //   setProfilePayload((prevProfileData) => {
+  //     return {
+  //       ...prevProfileData,
+  //       [name]: value,
+  //     };
+  //   });
+  // };
+  // const handleSaveInfo = (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   handleToggleEdit();
+  //   editProfileData({ ...profilePayload, id: user?.id as string });
+  // };
+  console.log(full_name);
+
+  const onSubmit = (fieldValues: ProfileFormValues) => {
+    console.log(fieldValues);
     handleToggleEdit();
-    editProfileData({ ...profilePayload, id: user?.id as string });
+    editProfileData({ ...fieldValues, id: user?.id as string });
   };
 
   const handleLogOut = () => {
     mutateLogout();
   };
+
   useEffect(() => {
     const avatar = createAvatar(avataaars, {
-      seed: profilePayload?.full_name,
+      seed: full_name,
     });
     const avatarSrc = avatar.toDataUri();
 
-    if (profilePayload?.full_name) {
-      setProfilePayload((prev) => ({ ...prev, avatar_url: avatarSrc }));
+    if (full_name) {
+      reset((prev) => ({ ...prev, avatar_url: avatarSrc }));
     }
-  }, [profilePayload?.full_name]);
+  }, [full_name, reset]);
+  console.log(full_name);
   return (
     <div className="mx-auto max-w-lg flex-grow px-4 py-8">
       <Card className="px-4 py-4">
@@ -104,9 +126,9 @@ const ProfilePage = () => {
           <div className="flex items-center gap-14">
             <Label className="mb-2 w-24">{t("profile-page.photo-label")}</Label>
             <div className="flex h-20 w-20 flex-col rounded-lg bg-slate-500 p-1">
-              {profilePayload.avatar_url && (
+              {avatar_url && (
                 <img
-                  src={profilePayload.avatar_url ?? ""}
+                  src={avatar_url ?? ""}
                   className="rounded-lg"
                   alt="avatar-pic"
                 />
@@ -123,7 +145,7 @@ const ProfilePage = () => {
             <UserRoundPen className="h-6 w-8 text-green-600" />
           </Button>
         </div>
-        <form className="space-y-9 px-4 pb-8 pt-1" onSubmit={handleSaveInfo}>
+        <div className="space-y-9 px-4 pb-8 pt-1">
           <Tabs defaultValue="geo" className="mx-auto">
             <TabsList className="light:bg-neutral-200 border-1 mx-auto mb-8 grid h-9 max-w-64 grid-cols-2 items-center justify-center rounded-lg p-1 text-muted-foreground">
               <TabsTrigger value="geo">{t("profile-page.tab-geo")}</TabsTrigger>
@@ -137,13 +159,25 @@ const ProfilePage = () => {
                 {!toggleEdit ? (
                   <p className="font-semibold text-green-600">{full_name_ka}</p>
                 ) : (
-                  <Input
-                    type="text"
-                    id="nameKa"
-                    className="max-w-56"
-                    value={full_name_ka}
+                  <Controller
                     name="full_name_ka"
-                    onChange={handleChange}
+                    control={control}
+                    // rules={{
+                    //   required: "Full Name is required",
+                    // }}
+                    render={({ field: { onChange, value } }) => {
+                      return (
+                        <>
+                          <Input
+                            type="text"
+                            id="nameKa"
+                            className="max-w-56"
+                            value={value}
+                            onChange={onChange}
+                          />
+                        </>
+                      );
+                    }}
                   />
                 )}
               </div>
@@ -156,13 +190,21 @@ const ProfilePage = () => {
                 {!toggleEdit ? (
                   <p className="font-semibold text-green-600">{full_name}</p>
                 ) : (
-                  <Input
-                    type="text"
-                    className="max-w-56"
-                    value={full_name}
+                  <Controller
                     name="full_name"
-                    onChange={handleChange}
-                  ></Input>
+                    control={control}
+                    render={({ field: { onChange, value } }) => {
+                      return (
+                        <Input
+                          type="text"
+                          id="name"
+                          className="max-w-56"
+                          value={value}
+                          onChange={onChange}
+                        />
+                      );
+                    }}
+                  />
                 )}
               </div>
             </TabsContent>
@@ -177,13 +219,20 @@ const ProfilePage = () => {
                 {avatar_url}
               </p>
             ) : (
-              <Input
-                type="url"
-                id="avatarUrl"
-                className="max-w-56"
-                value={avatar_url}
+              <Controller
                 name="avatar_url"
-                onChange={handleChange}
+                control={control}
+                render={({ field: { value, onChange } }) => {
+                  return (
+                    <Input
+                      type="url"
+                      id="avatarUrl"
+                      className="max-w-56"
+                      value={value}
+                      onChange={onChange}
+                    />
+                  );
+                }}
               />
             )}
           </div>
@@ -196,18 +245,30 @@ const ProfilePage = () => {
                 {phone_number}
               </p>
             ) : (
-              <Input
-                type="tel"
-                id="phoneNumber"
-                className="max-w-56"
-                value={phone_number}
+              <Controller
                 name="phone_number"
-                onChange={handleChange}
-              ></Input>
+                control={control}
+                render={({ field: { value, onChange } }) => {
+                  return (
+                    <Input
+                      type="tel"
+                      id="phoneNumber"
+                      className="max-w-56"
+                      value={value}
+                      onChange={onChange}
+                    />
+                  );
+                }}
+              />
             )}
           </div>
 
-          <Button className="w-full" disabled={!toggleEdit}>
+          <Button
+            type="submit"
+            className="w-full"
+            onClick={handleSubmit(onSubmit)}
+            disabled={!toggleEdit}
+          >
             {t("profile-page.edit-button")}
           </Button>
           <Button
@@ -217,7 +278,7 @@ const ProfilePage = () => {
           >
             {t("profile-page.sign-out-button")}
           </Button>
-        </form>
+        </div>
       </Card>
     </div>
   );
